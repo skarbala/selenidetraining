88cd1cc24990cc6f3876630d144a430e3dd19ff9
skarbala <skarbala.martin@gmail.com>
2019-08-13 20:45:12 +0200

    video 21 najdi element v liste podla textu

--- src/test/java/tests/SpelleologyTest.java
+++ src/test/java/tests/SpelleologyTest.java
@@ -1,22 +1,13 @@
 package tests;
 
 import base.TestBase;
-import com.codeborne.selenide.CollectionCondition;
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.FileNotFoundException;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.codeborne.selenide.CollectionCondition.*;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static utils.DataUtils.getExpectedSpells;
@@ -37,20 +28,16 @@ public class SpelleologyTest extends TestBase {
 
     @Test
     public void itShouldDisplayTortureSpell() {
-        ElementsCollection spellElements = $("ul.spells")
+        $("ul.spells")
                 .findAll("li")
-                .shouldHave(sizeGreaterThan(1));
+                .shouldHave(sizeGreaterThan(1))
+                .find(exactText("tortures a person"))
+                .click();
 
-        for (WebElement spellElement : spellElements) {
-            if (spellElement.getText().equals("tortures a person")) {
-                spellElement.click();
-            }
-        }
-        new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.modal-container")));
-        WebElement modal = $("div.modal-container");
-        Assert.assertTrue(modal.getText().contains("Crucio"));
-    }
+        $("div.modal-container")
+                .should(appear)
+                .shouldHave(text("Crucio"));
+     }
 
     @Test
     public void itShouldFilterSpells() {
